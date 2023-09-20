b997b0b9769bdc81c020b0b9ec14f8890937be37
sdi <sdi@goodit.kr>
2023-09-20 16:59:12 +0900

    23.09.20 Main_All_Menu_1

--- js/index.js
+++ js/index.js
@@ -1,10 +1,60 @@
+// gnb_menu
 const navH = document.querySelector(".gnb_menu_wrap");
 const head = document.querySelector("header");
 navH.addEventListener('mouseenter', () => {
-  // alert("hover");
   head.classList.add('active');
 })
 head.addEventListener('mouseleave', () => {
-  // alert("hover");
   head.classList.remove('active');
 })
+
+
+
+// all_menu(Mobile)
+$(document).ready(function(){
+  $("#all_menu_contain").hide();
+
+  $( '.all_menu_open' ).click( function() {
+    $( '#all_menu_contain' ).slideDown( 300, 'linear' );
+  } );
+
+  $( '.all_menu_close' ).click( function() {
+    $( '#all_menu_contain' ).slideUp( 300, 'linear' );
+  } );
+});
+
+
+$(window).resize(function(){ 
+  if (window.innerWidth > 1400) {  
+  
+  // 1400 이상 
+  // all_menu(Mobile)
+  $(document).ready(function(){
+    $(".all_menu_list_wrap ul.drop_down").show();
+    $(".all_menu_list_wrap li.dep_01").off("click"); 
+
+  });
+
+
+  } else {
+  
+    // 1400 이하 
+    // all_menu(Mobile)
+    $(document).ready(function(e){
+      $(".all_menu_list_wrap ul.drop_down").hide();
+      $(".all_menu_list_wrap li.dep_01").click(function(){
+        $("ul.drop_down",this).slideToggle("fast");
+        e.stopPropagation();
+      });
+    });
+
+
+  }
+  
+  }).resize(); 
+
+
+
+
+
+
